ccfb568782ad2fe3abb596d30e358c66bf1a8d79
SiddhuShkya <siddhuushakyaa@gmail.com>
2024-06-10 12:48:39 +0545

    added faiss-cpu in the requirements

--- Chat.py
+++ Chat.py
@@ -12,8 +12,6 @@ import google.generativeai as genai
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 
 
-load_dotenv()
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
     
 class Chat:
     
